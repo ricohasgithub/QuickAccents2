ffb0a255775ad7f58f507b8d8ea57eb54f8e0b36
ricohasgithub <ricozhuthegreat@hotmail.com>
2021-08-03 17:12:37 -0400

    Added Message Passing

--- background.js
+++ background.js
@@ -2,18 +2,8 @@
 // Listen for hotkey shortcut command
 chrome.commands.onCommand.addListener((e_grave) => {
     console.log("Hello world!");
-    navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
-        if (result.state === 'granted') {
-            let blob = new Blob(['hello'], {type: 'text/plain'});
-            let item = new ClipboardItem({'text/plain': blob});
-            navigator.clipboard.write([item]).then(function() {
-              console.log("Copied to clipboard successfully!");
-            }, function(error) {
-              console.error("unable to write to clipboard. Error:");
-              console.log(error);
-            });
-        } else {
-            console.log("clipboard-permissoin not granted: " + result);
-        }
+    chrome.runtime.sendMessage({
+        type: 'copy',
+        text: 'some text to copy'
     });
 });
--- src/js/popup.js
+++ src/js/popup.js
@@ -135,3 +135,16 @@ function clearRoot () {
   }
 
 }
+
+chrome.runtime.onMessage.addListener(function(message) {
+  console.log("COPY REQUEST RECEIVED")
+  if (message && message.type == 'copy') {
+      let input = document.createElement('textarea');
+      document.body.appendChild(input);
+      input.value = message.text;
+      input.focus();
+      input.select();
+      document.execCommand('Copy');
+      input.remove();
+  }
+});
